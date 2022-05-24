3ff86dd1b2375a0c8abbfdfac1efb88da5bc3e70
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-05-24 20:35:55 +0000

    added accesors to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :owner
   belongs_to :category
+  has_many(:interactions, { :class_name => "Interaction", :foreign_key => "item_id", :dependent => :destroy })
+  belongs_to(:category, { :required => true, :class_name => "Category", :foreign_key => "category_id", :counter_cache => true })
+  belongs_to(:author, { :required => true, :class_name => "User", :foreign_key => "owner_id" })
 end
