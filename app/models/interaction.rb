2f8c3124fa21fadab968dfb8dd05eb6007f768a2
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-05-24 20:37:45 +0000

    added interactions table

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -1,4 +1,6 @@
 class Interaction < ApplicationRecord
-  belongs_to :item
-  belongs_to :user
+
+
+  belongs_to(:item, { :required => true, :class_name => "Item", :foreign_key => "item_id", :counter_cache => true })
+  belongs_to(:user, { :required => true, :class_name => "User", :foreign_key => "user_id", :counter_cache => true })
 end
