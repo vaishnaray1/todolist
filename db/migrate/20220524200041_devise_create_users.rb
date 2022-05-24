85acc946ea56da7827619e4234662d0f6ff362f2
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-05-24 20:07:28 +0000

    added defaults to users table

--- db/migrate/20220524200041_devise_create_users.rb
+++ db/migrate/20220524200041_devise_create_users.rb
@@ -2,6 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
+    enable_extension("citext")
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -32,16 +33,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
-      t.integer :items_count
+      # t.string :username
+      t.citext :username
+      t.integer :items_count, default: 0
       t.boolean :private
-      t.integer :tasks_count
+      t.integer :tasks_count, default: 0
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
