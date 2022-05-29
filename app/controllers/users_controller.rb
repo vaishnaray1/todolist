3f0ab063807720ec93a28d709f708e999bdd8a16
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-05-29 05:31:49 +0000

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def followers
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def following
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
-  
+  root to: "items#index"
 
   resources :categories
   resources :interactions
   resources :items
   devise_for :users
   
+  get ":username" => "users#show", as: :user
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "items#index"
+  
+
 
 end
