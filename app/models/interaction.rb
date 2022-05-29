50a391f479a2e068695f8d1e20bdc03995f533b3
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-05-29 15:53:29 +0000

    updated counter cache

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -20,6 +20,6 @@
 #
 class Interaction < ApplicationRecord
   belongs_to :item, class_name: "Item", counter_cache: true
-  belongs_to :user, class_name: "User", counter_cache: true
+  belongs_to :user, class_name: "User"
  
 end
