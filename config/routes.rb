18887ebe52e1a11b83c9115110846c2590d07246
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-06-02 01:36:23 +0000

    add js

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   get ":username" => "users#show", as: :user
+
+  delete ":item/completed" => "items#notcompleted", as: :deletecompleted
+
+  get ":item/completed" => "items#completed", as: :getcompleted
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
